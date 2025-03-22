bd61ae5047947319877d20caa09c7e7409c7ca65
Le Van Sy <syle190104@gmail.com>
2025-03-22 16:55:27 +0700

    fetch all user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,81 +1,60 @@
 
 import { Space, Table, Tag } from 'antd';
+import { fetchAllUserAPI } from '../../services/api.services';
+import { useEffect, useState } from 'react';
 const UserTable = () => {
+
+    const [dataUsers, setDataUsers] = useState([
+        { id: 12, name: "sy", email: "ád@gmail.com", age: 12, gender: "MALE", address: "q12" },
+        { id: 4, name: "syle", email: "ádád@gmail.com", age: 12, gender: "MALE", address: "q12" },
+    ]);
+
+    useEffect(() => {
+        console.log(">>>run effect")
+        loadUser();
+    }, []);
 
     const columns = [
         {
+            title: 'ID',
+            dataIndex: 'id',
+        },
+        {
             title: 'Name',
             dataIndex: 'name',
-            key: 'name',
-            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
         },
         {
             title: 'Age',
             dataIndex: 'age',
-            key: 'age',
+        },
+        {
+            title: 'Gender',
+            dataIndex: 'gender',
         },
         {
             title: 'Address',
             dataIndex: 'address',
-            key: 'address',
         },
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_, { tags }) => (
-                <>
-                    {tags.map((tag) => {
-                        let color = tag.length > 5 ? 'geekblue' : 'green';
-                        if (tag === 'loser') {
-                            color = 'volcano';
-                        }
-                        return (
-                            <Tag color={color} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    })}
-                </>
-            ),
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (_, record) => (
-                <Space size="middle">
-                    <a>Invite {record.name}</a>
-                    <a>Delete</a>
-                </Space>
-            ),
-        },
-    ];
-    const data = [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            tags: ['nice', 'developer'],
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sydney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
-        },
+
     ];
 
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI();
+        setDataUsers(res.data);
+    }
+
+
+
     return (
-        <Table columns={columns} dataSource={data} />
+        <Table
+            columns={columns}
+            dataSource={dataUsers}
+            rowKey="id"
+        />
     )
 }
 export default UserTable;
--- src/services/api.services.js
+++ src/services/api.services.js
@@ -10,7 +10,16 @@ const createUserAPI = (name, email, password, age, gender, address) => {
         gender: gender,
         address: address
     }
+
     return axios.post(URL_BACKEND, data)
 }
 
-export { createUserAPI };
+const fetchAllUserAPI = () => {
+    const URL_BACKEND = "/api/v1/users/fetchAll";
+    return axios.get(URL_BACKEND);
+}
+
+export {
+    createUserAPI,
+    fetchAllUserAPI
+};
